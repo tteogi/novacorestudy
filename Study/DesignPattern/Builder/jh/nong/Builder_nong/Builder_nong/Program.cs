7cd409863e05fd8b821c477da7ea6cdeac0bdedb
poppy <jonghoon17@gmail.com>
2020-07-14 10:47:37 +0900

    [옥종훈] 빌드패턴 예제

---
+++ Study/DesignPattern/Builder/jh/nong/Builder_nong/Builder_nong/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+namespace Builder_nong
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            UnitBuilder[] units;
+
+            units = new UnitBuilder[3]; 
+            new UnitBuilder();
+
+            units[0] = new UnitBuilder().setWeapon("총").setNick("종훈의총잡이").setSkin("파란스킨");
+            units[1] = new UnitBuilder().setWeapon("칼").setNick("종훈의칼").setSkin("초록스킨");
+            units[2] = new UnitBuilder().setWeapon("망치").setNick("종훈의망치").setSkin("빨강스킨");
+
+            foreach(var unit in units)
+            {
+                unit.PrintAllEquip();
+            }
+        }
+    }
+}
