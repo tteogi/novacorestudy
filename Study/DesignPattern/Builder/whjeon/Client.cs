414c321b295da2aac85e43adf4cb547418cf8e8b
whjeon <cumaskr@naver.com>
2020-07-14 10:34:32 +0900

    [전우형] 빌더패턴 예제

---
+++ Study/DesignPattern/Builder/whjeon/Client.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System;
+
+namespace DesignPattern.Builder.whjeon
+{
+    public class NetClient
+    {
+        private byte[] _buffer { get; set; }
+
+        private Socket _socket { get; set; }
+
+        private IPEndPoint _localEndPoint { get; set; }
+
+        public class Builder
+        {
+            public Socket Socket { get; set; }
+
+            public string Address { get; set; }
+
+            public string Port { get; set; }
+
+            public IPEndPoint LocalEndPoint { get; set; }
+
+            public Builder Init()
+            {
+                Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                return this;
+            }
+            public Builder AddressSet(string address, int port)
+            {
+                LocalEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                return this;
+            }
+            public NetClient Build()
+            {
+                return new NetClient(this);
+            }
+        }
+
+        private NetClient(Builder builder)
+        {
+            _socket = builder.Socket;
+            _localEndPoint = builder.LocalEndPoint;
+        }
+
+        public bool Connect()
+        {
+            if (_localEndPoint == null)
+                return false;
+            try
+            {
+                _socket.Connect(_localEndPoint);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            NetClient.Builder builder = new NetClient.Builder();
+            NetClient client = builder.Init().AddressSet("127.0.0.1", 9999).Build();
+            if (client.Connect() == false)
+            {
+                Console.WriteLine("Connect Fail");
+                return;
+            }
+
+            Console.WriteLine("Connect Success");
+        }
+    }
+}
---
+++ Study/DesignPattern/Builder/whjeon/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DesignPattern.Builder.whjeon
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Socket socket;
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 9999));
+            socket.Listen(100);
+
+            Console.WriteLine("소켓대기");
+
+            Socket accepted = socket.Accept();
+
+            Console.WriteLine("소켓연결");
+
+            accepted.Close();
+
+            socket.Close();
+        }
+    }
+}
